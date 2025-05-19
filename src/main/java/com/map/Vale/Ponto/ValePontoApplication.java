2879d7cfaa9251388d086df036db9fc110b7f571
gabrielcaio11 <gabrielcaio848@gmail.com>
2025-05-19 18:45:01 -0300

    config initial

---
+++ src/main/java/com/map/Vale/Ponto/ValePontoApplication.java
@@ -0,0 +1,13 @@
+package com.map.Vale.Ponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValePontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValePontoApplication.class, args);
+	}
+
+}
